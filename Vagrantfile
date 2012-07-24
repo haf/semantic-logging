89da1155c6fe5f2b0bb06ef2ce0ce0c2d97fbdb7
Henrik <henrik@haf.se>
2012-07-24 23:47:06 +0200

    something pretty much with nginx

--- Vagrantfile
+++ Vagrantfile
@@ -9,9 +9,11 @@ Vagrant::Config.run do |config|
   config.vm.forward_port 9292, 9292 # logstash default web
   config.vm.forward_port 5672, 5672 # rabbitmq
   config.vm.forward_port 9200, 9200 # elasticsearch
-  # config.vm.forward_port 80, 8080 # kibana
+  config.vm.forward_port 80, 8080 # kibana
+  config.vm.forward_port 55672, 55672 # rabbitmq management
 
-  config.vm.share_folder "puppet-modules", "/etc/puppet/modules", "./modules"
+  # config.vm.share_folder "puppet-modules", "/etc/puppet/modules", "./modules"
+  config.vm.share_folder "puppet-files", "/etc/puppet/files", "./files"
   
   config.vm.customize do |vm|
     vm.memory_size = 1024
@@ -21,7 +23,7 @@ Vagrant::Config.run do |config|
     puppet.module_path = "./modules"
     puppet.manifests_path = "."
     puppet.manifest_file = "manifests/semantic-logging.pp"
-    puppet.options = [ "--verbose", "--debug" ]
+    puppet.options = ["--fileserverconfig=/vagrant/fileserver.conf", "--verbose", "--debug" ]
   end
 
   
--- manifests/semantic-logging.pp
+++ manifests/semantic-logging.pp
@@ -15,18 +15,107 @@ newer ${sentinel})\"",
   }
 }
 
+class kibana {
+
+  $kpath = '/tmp/kibana.tar.gz'
+  $wwwpath = '/var/www/kibana'
+
+  file { "$kpath":
+    source => 'puppet:///files/rashidkpc-Kibana-v0.1.5-44-gf5c80c3.tar.gz',
+    ensure => present
+  }
+  
+  exec { "$kpath":
+    command => "tar -zxvf $kpath",
+    unless  => "find $wwwpath",
+    creates => "$wwwpath",
+    require => [
+      File["$kpath"],
+      File['/var/www']
+    ],
+    cwd     => '/var/www/',
+    path    => "/usr/local/bin:/usr/sbin:/usr/bin:/bin"
+  }
+
+  package { 'php':
+    name => ["php5-cli", "php5-common", "php5-suhosin", "php5-curl"],
+    ensure => present
+  }
+
+  package { 'php-fpm':
+    name => [ "php5-fpm", "php5-cgi" ],
+    ensure => present,
+    require => Package['php']
+  }
+  
+  file { "/var/www":
+    ensure => "directory",
+    owner  => "root",
+    group  => "root",
+    mode   => '0755'
+  }
+
+  file { "$wwwpath":
+    ensure => "directory",
+    owner => 'kibana',
+    group => 'kibana',
+    mode => '0755',
+    require => Exec["$kpath"]
+  }
+
+  group { 'kibana':
+    ensure => present
+  }
+  
+  user { 'kibana':
+    ensure => present,
+    gid => 'kibana',
+    home => '',
+    managehome => true,
+    require => Group['kibana'] 
+  }
+  
+  service { 'php5-fpm':
+  	ensure => running,
+  	enable => true,
+  	hasrestart => true,
+  	hasstatus => true,
+  	require => Package['php-fpm']
+  }
+
+  include nginx
+  include nginx::fcgi
+  
+  nginx::site { 'nginx-default-disable':
+    ensure => absent
+  }
+
+  nginx::fcgi::site { 'nginx-kibana-enable':
+    root            => "$wwwpath",
+    fastcgi_pass    => "127.0.0.1:9000",
+    server_name     => ["localhost", "$::hostname", "$::fqdn"],
+    require         => Service['php5-fpm'],
+    template        => 'nginx/fcgi_site.erb',
+    listen          => '80'
+  }
+}
+
 node 'semantic-logging' {
   class { 'apt_get_update':
     stage => pre
   }
+  
   include rabbitmq
   rabbitmq::vhost { '/': }
   rabbitmq::plugin { 'rabbitmq_management': }
+  
   class { 'elasticsearch':
     version      => '0.19.8',
     java_package => 'openjdk-6-jre-headless',
     dbdir        => '/var/lib/elasticsearch',
     logdir       => '/var/log/elasticsearch',
   }
+  
   class { 'logstash': }
+  class { 'kibana': }
 }
